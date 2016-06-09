98ef391120b0b94a4133f11ba27cb86cff95d53f
Airbulls23 <fabian@mett-berlin.de>
2016-06-09 15:40:25 +0200

    Update trajCount.py
    COworker: Tony

--- mcmm/trajCount.py
+++ mcmm/trajCount.py
@@ -1,1 +1,27 @@
-#here comes the slidingWindowCount(int [] traj, int tau) function
+def slidingWindowCount(traj,tau):
+    """
+    returns the countmatrix, gets a trajectory and tau
+    
+    Paramaters
+    ----------
+    traj: arraylike
+    tau : integer
+    ----------
+    
+    Return
+    ------
+    Countmatrix: matrixlike
+    ------
+    """
+    n = np.max(traj)+ 1
+    Countmatrix = np.zeros((n,n))
+    T = len(traj) 
+    
+    for i in range(n):
+        for j in range(n):
+            summe = 0
+            for k in range(T- tau ):
+                if traj[k] == i and traj[k+tau] == j:
+                    summe +=1
+            Countmatrix[i][j] = summe
+    return Countmatrix
