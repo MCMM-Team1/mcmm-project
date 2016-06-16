6662ac65dea573e03af0911d08d7b42a3aef3a46
Tony <tony.schwedek@googlemail.com>
2016-06-16 14:32:12 +0200

    added the msm class from the MCMM notebook from palmperez

--- mcmm/msm.py
+++ mcmm/msm.py
@@ -1,4 +1,8 @@
 import numpy as np
+import scipy.linalg 
+import matplotlib.pyplot as plt
+
+
 
 def DFS(D,v,E,label = []):
 	"""
@@ -72,3 +76,56 @@ def kosaraju(T):
 		for i in C:
 			V.remove(i)
 	return CC
+
+class msm(object):  
+    def __init__(self,P):
+        """Initial step: Checks if the matrix is stochastic and quadratic"""
+        for i in range(len(P[0])):
+            if (abs(np.sum(P[i])-1>1.0E-8)):
+                print "Not stochastic"
+        if (len(P[0])!=len(P[1])):
+            print "Not quadratic"
+    
+    
+    def eigAnalysis(self,P):
+        """Calculates eigenvalus and eigevectors both right and left.
+        
+        Input: Stochastic (int(n),int(n)) matrix.
+        
+        Output: Array with eigenvalus sorted in descending order.
+                Left and right eigenvectors normlaized in the 2-norm v[:,i].
+                Stationary distribution as probability distribution as an array."""
+        eigval , eigvec_right , eigvec_left = scipy.linalg.eig(P,left=True,right=True)
+
+        eigval = np.real(eigval)
+        eigvec_right = np.real(eigvec_right)
+        eigvec_left = np.real(eigvec_left)
+
+        idx = np.argsort(abs(eigval))[::-1]
+
+        eigval = eigval[idx]
+        eigvec_left = eigvec_left[:,idx]
+        eigvec_right = eigvec_right[:,idx]
+        
+        self.eigval = eigval
+        self.eigvec_left = eigvec_left 
+        self.eigvec_right = eigvec_right
+        self.stationary = eigvec_right[:,0]/np.sum(eigvec_right[:,0])
+        return self
+    
+    def visualizeMatrix(self,P):
+        """Plots the transition matrix as a 2D plot"""
+        fig = plt.figure(figsize=(6, 3.2))
+        ax = fig.add_subplot(111)
+        ax.set_title('colorMap')
+        plt.imshow(P)
+        ax.set_aspect('equal')
+
+        cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+        cax.get_xaxis().set_visible(False)
+        cax.get_yaxis().set_visible(False)
+        cax.patch.set_alpha(0)
+        cax.set_frame_on(False)
+        plt.colorbar(orientation='vertical')
+        
+        return plt.show()
