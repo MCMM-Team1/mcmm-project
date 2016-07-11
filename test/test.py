2a4210cee8184f85208e3c04a5e80911f20ccc7f
Fabian Mett <fabian@mett-berlin.de>
2016-07-11 13:38:52 +0200

    fixed tests

--- test/test.py
+++ test/test.py
@@ -23,6 +23,7 @@ def checkTolerance(m1,m2,tolerance):
     """
     checks if the diffrence between the entries of the matrices m1 and m2 is less than tolerance
     """
+
     for i in range(len(m1)):
         for j in range(len(m2)):
             if (np.absolute(m1[i][j]-m2[i][j])>=tolerance):
@@ -93,26 +94,26 @@ class TestStringMethods(unittest.TestCase):
         t8 = np.array([[0.3,0.2,0.3,0.2],[0.05,0.8,0.05,0.1],[0.4,0.1,0.3,0.2],[0.25,0.25,0.25,0.25]])
         
         #create trajectories of length 10000, start once in each state
-        t61 = np.zeros(10000)
-        t62 = np.zeros(10000)
-        t63 = np.zeros(10000)
+        t61 = np.zeros(10000,dtype = np.int8)
+        t62 = np.zeros(10000,dtype = np.int8)
+        t63 = np.zeros(10000,dtype = np.int8)
         t61[0]=0
         t62[0]=1
         t63[0]=2
         
-        t71 = np.zeros(10000)
-        t72 = np.zeros(10000)
-        t73 = np.zeros(10000)
-        t74 = np.zeros(10000)
+        t71 = np.zeros(10000,dtype = np.int8)
+        t72 = np.zeros(10000,dtype = np.int8)
+        t73 = np.zeros(10000,dtype = np.int8)
+        t74 = np.zeros(10000,dtype = np.int8)
         t71[0]=0
         t72[0]=1
         t73[0]=2
         t74[0]=3
         
-        t81 = np.zeros(10000)
-        t82 = np.zeros(10000)
-        t83 = np.zeros(10000)
-        t84 = np.zeros(10000)
+        t81 = np.zeros(10000,dtype = np.int8)
+        t82 = np.zeros(10000,dtype = np.int8)
+        t83 = np.zeros(10000,dtype = np.int8)
+        t84 = np.zeros(10000,dtype = np.int8)
         t81[0]=0
         t82[0]=1
         t83[0]=2
@@ -167,20 +168,19 @@ class TestStringMethods(unittest.TestCase):
             
             
         #create the count matrices
-        c61 = mcmm.trajCount.slidingWindowCount(t61,1)
-        c62 = mcmm.trajCount.slidingWindowCount(t62,1)
-        c63 = mcmm.trajCount.slidingWindowCount(t63,1)
+        c61 = mcmm.trajCount.slidingWindowCount(t61,1,max(t61)+1)
+        c62 = mcmm.trajCount.slidingWindowCount(t62,1,max(t62)+1)
+        c63 = mcmm.trajCount.slidingWindowCount(t63,1,max(t63)+1)
         
-        c71 = mcmm.trajCount.slidingWindowCount(t71,1)
-        c72 = mcmm.trajCount.slidingWindowCount(t72,1)
-        c73 = mcmm.trajCount.slidingWindowCount(t73,1)
-        c74 = mcmm.trajCount.slidingWindowCount(t74,1)
+        c71 = mcmm.trajCount.slidingWindowCount(t71,1,max(t71)+1)
+        c72 = mcmm.trajCount.slidingWindowCount(t72,1,max(t72)+1)
+        c73 = mcmm.trajCount.slidingWindowCount(t73,1,max(t73)+1)
+        c74 = mcmm.trajCount.slidingWindowCount(t74,1,max(t74)+1)
         
-        c81 = mcmm.trajCount.slidingWindowCount(t81,1)
-        c82 = mcmm.trajCount.slidingWindowCount(t82,1)
-        c83 = mcmm.trajCount.slidingWindowCount(t83,1)
-        c84 = mcmm.trajCount.slidingWindowCount(t84,1)
-        
+        c81 = mcmm.trajCount.slidingWindowCount(t81,1,max(t81)+1)
+        c82 = mcmm.trajCount.slidingWindowCount(t82,1,max(t82)+1)
+        c83 = mcmm.trajCount.slidingWindowCount(t83,1,max(t83)+1)
+        c84 = mcmm.trajCount.slidingWindowCount(t84,1,max(t84)+1)
         assert_true(checkTolerance(t6,mcmm.countmatrixTransitionmatrix.nonrevTmatrix(c61),tolerance))
         assert_true(checkTolerance(t6,mcmm.countmatrixTransitionmatrix.nonrevTmatrix(c62),tolerance))
         assert_true(checkTolerance(t6,mcmm.countmatrixTransitionmatrix.nonrevTmatrix(c63),tolerance))
@@ -198,17 +198,17 @@ class TestStringMethods(unittest.TestCase):
         assert_true(checkTolerance(t8,mcmm.countmatrixTransitionmatrix.nonrevTmatrix(c84),tolerance)) 
 
     def test_reducedCountMatrix(self):
-        bigMatrix = np.array([[1,2,0,3,0,4,5],[6,7,0,8,0,9,10],[5,5,5,5,5,5,5],[11,12,0,13,0,14,15],[5,5,5,5,5,5,5],16,17,0,18,0,19,20],[21,22,0,23,0,24,25]])
+        bigMatrix = np.array([[1,2,0,3,0,4,5],[6,7,0,8,0,9,10],[5,5,5,5,5,5,5],[11,12,0,13,0,14,15],[5,5,5,5,5,5,5],[16,17,0,18,0,19,20],[21,22,0,23,0,24,25]])
         dictionary = {0:0 , 1:1 , 2:3 , 3:5 , 4:6}
         smallMatrix = np.array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]])
         
-        indices = mcmm.msm.findLargestCommClass(bigMatrix)
+        indices = np.sort(mcmm.msm.findLargestCommClass(bigMatrix))
         resultdictionary = {i:indices[i] for i in range(len(indices))}
         result = np.zeros((len(indices),len(indices)),dtype = np.int8)
         for i in range(len(indices)):
             for j in range(len(indices)):
                 result[i][j] = bigMatrix[resultdictionary[i],resultdictionary[j]]
-        assert_true(smallMatrix,np.sort(result))
-        assert_true(dictionary,resultdictionary)
+        assert_true((smallMatrix == result).all())
+        assert_true(dictionary == resultdictionary)
 if __name__ == '__main__':
     unittest.main()
